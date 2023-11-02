5e07cb36e1a7568ab87adfe9471679ab693f35ec
Imunguambe <isaiasruben971@gmail.com>
2023-11-02 21:44:04 +0200

    first read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,34 @@
-#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
 
- */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	if(filename ==NUll)
+	{
+		return 0;
+	}
+	ssize_t bytes_read = read(file_descriptor, buffer, letters);
+	
+   	if (bytes_read == -1) {
+        close(file_descriptor);
+        free(buffer);
+        return 0;
+    	}
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+   	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	free(buf);
-	close(fd);
-	return (w);
+    	if (bytes_written == -1 || bytes_written != bytes_read) {
+        close(file_descriptor);
+        free(buffer);
+        return 0;
+       }
+
+       close(file_descriptor);
+      free(buffer);
+
+    return bytes_written;
 }
 
             
