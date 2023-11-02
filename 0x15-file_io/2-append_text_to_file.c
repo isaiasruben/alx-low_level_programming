0302979aa0dad922249285a7f656c50d6814a7ee
Imunguambe <isaiasruben971@gmail.com>
2023-11-02 22:00:31 +0200

    second exercises

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,25 @@
 #include "main.h"
 
-int append_text_to_file(const char *filename, char *text_content) {
-    int o, w, len = 0;
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);  // Missing filename error
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL) {
-        for (len = 0; text_content[len]; len++);
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    o = open(filename, O_WRONLY | O_APPEND);
-    if (o == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
-        return (-1);  // Failed to open the file
-    }
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-    w = write(o, text_content, len);
-    if (w == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
-        close(o);
-        return (-1);  // Failed to write to the file
-    }
+	if (o == -1 || w == -1)
+		return (-1);
 
-    close(o);
+	close(o);
 
-    return (1);  // Success
+	return (1);
 }
-
